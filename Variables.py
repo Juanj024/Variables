f1916dcf92f55d09183aba8b1e321ce13bf28a72
Juanj024 <Juanjok247@gmail.com>
2024-08-31 23:57:58 -0400

    Variables assignment

---
+++ Variables.py
@@ -0,0 +1,15 @@
+#Writting the inputs, all the operations and saving them in a variable
+result1= 5*6
+result2= 8/3
+result3= 8//3
+result4= 8%3
+result5= 4200000+23090
+result6= 4200000-7633
+
+#Printing all the variables on front of the message
+print("The answer to your query is {}" .format (result1))
+print("The answer to your query is {}" .format (result2))
+print("The answer to your query is {}" .format (result3))
+print("The answer to your query is {}" .format (result4))
+print("The answer to your query is {}" .format (result5))
+print("The answer to your query is {}" .format (result6))
